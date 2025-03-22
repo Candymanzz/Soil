9f2da0c93c619239ceaae21977c3eeeac94e89d7
Publo <111525871+Candymanzz@users.noreply.github.com>
2025-03-22 16:44:21 +0300

    add contracts

---
+++ server/Contracts/IUserService.cs
@@ -0,0 +1,14 @@
+using server.DTOs;
+
+namespace server.Contracts
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<UserDto>> GetAllUsersAsync();
+        Task<UserDto?> GetUserByIdAsync(int id);
+        Task<UserDto> CreateUserAsync(CreateUserDto createUserDto);
+        Task<UserDto> UpdateUserAsync(int id, UpdateUserDto updateUserDto);
+        Task DeleteUserAsync(int id);
+        Task<UserDto?> GetUserByUsernameAsync(string username);
+    }
+}
